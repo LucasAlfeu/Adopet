d298c9dba3c6844f3729511aac3d7c5ca9e2524c
Lucas Alfeu <lucas.a1feu@gmail.com>
2022-12-05 08:05:08 -0300

    Adicionando a funcionalidade de ir para a página de profile com o click no icone do usuário

--- src/components/ImageProfile/index.tsx
+++ src/components/ImageProfile/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import user from 'assets/user.svg';
 import styles from './ImageProfile.module.scss';
+import { useNavigate } from 'react-router-dom'
 
 export default function ImageProfile() {
-    return <img className={styles.imageProfile} src={user} alt="Imagem do usuário" />
+    const navigate = useNavigate()
+    return <img className={styles.imageProfile} onClick={()=> navigate('/profile')} src={user} alt="Imagem do usuário" />
 }
